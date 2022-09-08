65231181c834f79a4612f9d3390c198750b68660
nicolanaz <nicola.dzr@gmail.com>
2022-09-08 13:02:49 +0300

    main commit

--- src/main/java/code/Main.java
+++ src/main/java/code/Main.java
@@ -6,11 +6,14 @@ public class Main {
         map.put(1, "one");
         map.put(2, "two");
         map.put(3, "three");
+        map.put(4, "four");
+        map.put(5, "five");
 
         System.out.println(map.containsKey(1));
         System.out.println(map.containsValue("two"));
         System.out.println(map.getValue(3));
         System.out.println(map.getKey("one"));
+        System.out.println();
 
         for (MyMap<Integer, String>.Entry<Integer, String> entry : map.entrySet()) {
             System.out.println(entry);
